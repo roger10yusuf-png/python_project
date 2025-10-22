cbb87aa3d6f35dc1e1388b1b861c8a4f80c51604
roger10yusuf-png <roger10yusuf@gmail.com>
2025-10-22 17:40:58 +0200

    Add files via upload

--- pop_the_baloon.py
+++ pop_the_baloon.py
@@ -32,15 +32,28 @@ def balloons():
     balloon = turtle.Turtle()
     balloon.hideturtle()
     balloon.shape("circle")
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b = random.randint(0,255)
+    r = random.randint(1,255)
+    g = random.randint(1,255)
+    b = random.randint(1,255)
     balloon.color(r,g,b)
     balloon.penup()
     balloon.goto(random.randint(-280, 280), 280)
     balloon.showturtle()
     return balloon
 balloon = balloons()
+
+def imp_balloons():
+    imp_balloon = turtle.Turtle()
+    imp_balloon.hideturtle()
+    imp_balloon.shape("circle")
+    imp_balloon.color("black")
+    imp_balloon.penup()
+    imp_balloon.goto(random.randint(-280, 280), 280)
+    imp_balloon.showturtle()
+    return imp_balloon
+
+
+
 
 score = 0
 pen = turtle.Turtle()
@@ -54,12 +67,27 @@ while True:
     balloon.sety(y)
     if balloon.ycor() <= -280:
      balloon.hideturtle()
-     balloon = balloons()
+     if random.randint(1,10) == 7:
+            balloon = imp_balloons()
+     else:
+            balloon = balloons()
+            
     if balloon.distance(triangle) <= 10:
+        print(balloon.color())
         balloon.hideturtle()
+        if "black" == balloon.color()[0]:
+            pen.goto(-150,0)
+            pen.write("game over",font = ("arial", 70))
+            break
         score = score + 1
         pen.clear()
         pen.write("Score:" + str(score),font = ("arial", 20))
+        if random.randint(1,10) == 7:
+            balloon = imp_balloons()
+        else:
+            balloon = balloons()
+
+    
 
 
     
