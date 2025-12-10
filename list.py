31e01312d75ce704f48c484b4f976734c16b078c
roger10yusuf-png <roger10yusuf@gmail.com>
2025-12-10 18:00:32 +0100

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+list = [["phone","key","console"],
+        ["glasses","books","pencil"],
+        ["bag","plug","braces"]]
+print(list[1][0])
+
+for i in range(len(list)):
+    for j in range(len(list[i])):
+        print(list[i][j])
+
+for i in range(len(list)):
+    print(list[i][0])
+    
