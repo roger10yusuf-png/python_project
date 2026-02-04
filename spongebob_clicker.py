b6bba796bef96f979f7e2d80b48e89e5c657c4e8
roger10yusuf-png <roger10yusuf@gmail.com>
2026-02-04 17:10:45 +0100

    Add files via upload

--- spongebob_clicker.py
+++ spongebob_clicker.py
@@ -3,12 +3,18 @@ import random
 HEIGHT = 343
 WIDTH = 600
 spongebob = Actor("spongebob")
-spongebob.pos = (100,300)
+spongebob.pos = (100,250)
+patrick = Actor("patrick_star")
+patrick.pos = (220,250)
 def draw():
     screen.blit("landscape1",(0,0))
     spongebob.draw()
+    patrick.draw()
 def on_mouse_down(pos):
     if spongebob.collidepoint(pos):
         spongebob.x = (random.randint(0,600))
         spongebob.y = (random.randint(0,343))
-pgzrun.go()
+
+    patrick.pos = pos
+        
+pgzrun.go()                                 
