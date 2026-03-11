d5d456ff971c31e5d25d28ee019529fc6ef39f92
roger10yusuf-png <roger10yusuf@gmail.com>
2026-03-11 17:56:29 +0100

    Add files via upload

--- fruit_collector.py
+++ fruit_collector.py
@@ -11,18 +11,23 @@ pear = Actor("pear_fruit")
 banana = Actor("banana")
 
 
-
 def draw():
+    if score < 0:
+         screen.blit("game_over.png",(150,50)) 
+         return   
     screen.blit("grass.jpeg",(0,0))
     basket.draw()
     for fruit in fruit_list:
          fruit.draw()
+    for bomb in bomb_list:
+         bomb.draw()
+    screen.draw.text("score:"+str(score),center = (50,10))
+score = 0
 
 fruits = ["pear_fruit","banana","strawberry","orange"]
 
 fruit_list = []
-
-score = 0
+bomb_list = []
 
 def new_fruits():  
         fruit = Actor(random.choice(fruits))
@@ -31,8 +36,17 @@ def new_fruits():
         fruit_list.append(fruit)
         clock.schedule(new_fruits,random.randint(1,2))
 
+def imp_bomb():
+     bomb =  Actor("bomb")
+     bomb.x = random.randint(10,590)
+     bomb.y = 10
+     bomb_list.append(bomb)
+     clock.schedule(imp_bomb,random.randint(8,9))
+
 def update():
     global score
+    if score < 0:
+         return   
     if keyboard.a:
         basket.x += -2.5
     elif keyboard.d:
@@ -41,12 +55,19 @@ def update():
          fruit.y += 2.5
          if fruit.colliderect(basket):
                 fruit_list.remove(fruit)
+                score = score + 1
          elif fruit.y >= 399:
                 fruit_list.remove(fruit)
+    for bomb in bomb_list:
+         bomb.y += 2.5
+         if bomb.colliderect(basket):
+                bomb_list.remove(bomb)
+                score = score - 1
+
+            
         
-
-
 new_fruits()
+imp_bomb()
 
     
 
