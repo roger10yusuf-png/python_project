f131bf77d2659bf38c80154399c4aff926743266
roger10yusuf-png <roger10yusuf@gmail.com>
2026-06-10 17:50:56 +0200

    Add files via upload

---
+++ Product_class.py
@@ -0,0 +1,25 @@
+class Product():
+    def __init__(self,name,id,price,quantity):
+        self.name = name
+        self.id = id
+        self.price = price
+        self.quantity = quantity
+
+    def display(self):
+        print(self.name, self.id, self.price, self.quantity)
+
+    def check_in_stock(self,number_of_products):
+        if self.quantity >= number_of_products:
+            print("This product is available.")
+        elif self.quantity < number_of_products:
+            print("The requested amount of this product is unavailable. There are",self.quantity,"available.")
+        elif self.quantity == 0:
+            print("This product is out of stock.")
+
+product1 = Product("shoes",162,50.00,10)
+product1.display()
+product1.check_in_stock(12)
+
+        
+
+    
