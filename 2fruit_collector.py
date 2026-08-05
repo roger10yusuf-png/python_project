a40b01d1d240bb69e77cea8de8eb4fef71b60d97
roger10yusuf-png <roger10yusuf@gmail.com>
2026-08-05 17:37:27 +0200

    Add files via upload

--- 2fruit_collector.py
+++ 2fruit_collector.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 pygame.init()
 
@@ -7,6 +8,9 @@ screen = pygame.display.set_mode((800,600))
 
 background = pygame.image.load("images/fruitsy.png")
 background = pygame.transform.scale(background, (800,600))
+
+game_over_bg = pygame.image.load("images/game_over.png")
+game_over_bg = pygame.transform.scale(game_over_bg, (800,600))
 
 fruit_names = ["images/strawberry.png", "images/banana.png", "images/orange.png"]
 
@@ -22,10 +26,13 @@ class Basket(pygame.sprite.Sprite):
         self.image = pygame.image.load("basket.png")
         self.image = pygame.transform.scale(self.image, (250,150))
         self.rect = self.image.get_rect()
-        self.rect.center = [200,525]
-        self.speed = 8
+        self.rect.center = [400,525]
+        self.speed = 4
 
     def update(self):
+        global score
+        text1 = font.render("your score: "+str(score), True,(0,0,0))
+        screen.blit(text1,(10,10))
 
         keys = pygame.key.get_pressed()
 
@@ -34,7 +41,13 @@ class Basket(pygame.sprite.Sprite):
 
         if keys[pygame.K_d]:
             self.rect.x += self.speed
+        
+        collide = pygame.sprite.spritecollide(self, fruit_group, True)
+        for i in collide:
+            score = score + 1
+        
 
+lives = 3
 
 class Fruit(pygame.sprite.Sprite):
 
@@ -48,7 +61,14 @@ class Fruit(pygame.sprite.Sprite):
         self.rect.y = 0
 
     def update(self):
-        self.rect.y += 4
+        global lives
+        self.rect.y += 2.5
+        if self.rect.y >= 600:
+            lives = lives - 1
+            fruit_group.remove(self)
+            print(lives)
+        text2 = font.render("your lives: "+str(lives), True,(0,0,0))
+        screen.blit(text2,(10,55))
 
 
 
@@ -71,7 +91,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-    if currenttime - lasttime > 500:
+    if currenttime - lasttime > 750:
         fruit = Fruit(random.choice(fruit_names))
         fruit_group.add(fruit)
         lasttime = currenttime
@@ -79,6 +99,13 @@ while True:
     fruit_group.draw(screen)
     basket_group.draw(screen)
     basket_group.update()
+    if lives <= 0:
+        screen.blit(game_over_bg,(0,0))
+        pygame.display.update()
+        time.sleep(5)
+        pygame.quit()
+    
+
 
 
     pygame.display.update()
