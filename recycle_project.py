81ed49f85fde54018f51e204bda0b0c78fba71e1
roger10yusuf-png <roger10yusuf@gmail.com>
2026-08-26 17:56:03 +0200

    Add files via upload

---
+++ recycle_project.py
@@ -0,0 +1,51 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600,600))
+
+background = pygame.image.load("images/Glass_bg.jpeg")
+background = pygame.transform.scale(background, (600,600))
+
+class Bin(pygame.sprite.Sprite):
+
+    def __init__(self,x,y):
+        super().__init__()
+
+
+        self.image = pygame.image.load("images/trash_can.jpeg")
+        self.image = pygame.transform.scale(self.image, (70,70))
+        self.rect = self.image.get_rect()
+        self.rect.center = [x,y]
+        self.speed = 4
+
+    def update(self):
+        Keys = pygame.key.get_pressed()
+        if Keys[pygame.K_w]:
+            self.rect.y -= self.speed
+        elif Keys[pygame.K_a]:
+            self.rect.x -= self.speed
+        elif Keys[pygame.K_d]:
+            self.rect.x += self.speed
+        elif Keys[pygame.K_s]:
+            self.rect.y += self.speed
+
+bin = Bin(300,300)
+
+Bin_group = pygame.sprite.Group()
+Bin_group.add(bin)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    screen.blit(background, (0,0))
+    Bin_group.draw(screen)
+    Bin_group.update()
+    
+    
+    
+    
+    pygame.display.update()
+    
+
+    
