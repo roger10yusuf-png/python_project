c2ae933b58b83f20e8ce4aa3ee9eb3e98c68c972
roger10yusuf-png <roger10yusuf@gmail.com>
2026-09-09 18:02:23 +0200

    Add files via upload

--- recycle_project.py
+++ recycle_project.py
@@ -1,9 +1,15 @@
 import pygame
 import random
 
-images = ["pencil.png", "cardboard_box.png", "paper_bag.jpeg"]
+score = 0
+lives = 3
 
 pygame.init()
+
+font = pygame.font.SysFont("arial", 20)
+
+images = ["pencil.png", "cardboard_box.png", "paper_bag.png"]
+
 screen = pygame.display.set_mode((600,600))
 
 background = pygame.image.load("images/Glass_bg.jpeg")
@@ -22,6 +28,8 @@ class Bin(pygame.sprite.Sprite):
         self.speed = 4
 
     def update(self):
+        global score
+        global lives
         Keys = pygame.key.get_pressed()
         if Keys[pygame.K_w]:
             self.rect.y -= self.speed
@@ -31,8 +39,20 @@ class Bin(pygame.sprite.Sprite):
             self.rect.x += self.speed
         elif Keys[pygame.K_s]:
             self.rect.y += self.speed
+        if pygame.sprite.spritecollide(self, recyclable_group, True):
+             score = score + 1
+        if pygame.sprite.spritecollide(self, nonrecyclable_group, True):
+             lives = lives - 1
+        text = font.render("your lives: "+str(lives),True, (255,255,255))
+        screen.blit(text,(450,40))
+        text2 = font.render("your score "+str(score), True, (255,255,255))
+        screen.blit(text2, (450,10))
 
-bin = Bin(300,300)
+        
+
+
+
+bin = Bin(40,40)
 
 class Recyclable(pygame.sprite.Sprite):
 
@@ -46,18 +66,29 @@ class Recyclable(pygame.sprite.Sprite):
 
 
             
-
-recyclable = Recyclable(random.randint(50,550), random.randint(50,550))
-
 recyclable_group = pygame.sprite.Group()
-recyclable_group.add(recyclable)
+
+for i in range(20):
+    recyclable = Recyclable(random.randint(50,550), random.randint(50,550))
+    recyclable_group.add(recyclable)
 
 class Nonrecyclable(pygame.sprite.Sprite):
     
      def __init__(self,x,y):
             super().__init__()
 
-    
+            self.image = pygame.image.load("images/plastic.png")
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
+
+nonrecyclable_group = pygame.sprite.Group()
+
+for i in range(5):
+    nonrecyclable = Nonrecyclable(random.randint(50,550), random.randint(50,550)) 
+    nonrecyclable_group.add(nonrecyclable)
+
+
 
 Bin_group = pygame.sprite.Group()
 Bin_group.add(bin)
@@ -70,6 +101,7 @@ while True:
     Bin_group.draw(screen)
     Bin_group.update()
     recyclable_group.draw(screen)
+    nonrecyclable_group.draw(screen)
     
     
     
